dcbb9fbd05c3d1650c41b0ff41a8753452cd886d
tekangun <tekan.gun@gmail.com>
2021-02-06 18:31:22 +0300

    tekan

--- lib/core/locator.dart
+++ lib/core/locator.dart
@@ -1,9 +1,16 @@
-import 'package:flutter/cupertino.dart';
 import 'package:get_it/get_it.dart';
-import 'package:humantiy/screens/home.dart';
+import 'package:humantiy/core/services/data_services.dart';
+import 'package:humantiy/core/services/location_services.dart';
 GetIt getIt = GetIt.instance;
 
 void setUpLocators(){
+
+  getIt.registerLazySingleton(() => LocationServices());
+
+
+  getIt.registerFactoryParam<DataServices, String,String>(
+      (cityName,info) =>
+          DataServices(cityName: cityName));
 
 }
 
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:humantiy/constants.dart';
+import 'package:humantiy/core/locator.dart';
+import 'package:humantiy/core/services/data_services.dart';
+import 'package:humantiy/core/services/location_services.dart';
+import 'package:humantiy/models/air_data_model.dart';
 
 class Home extends StatefulWidget{
   @override
@@ -6,9 +11,21 @@ class Home extends StatefulWidget{
 }
 
 class HomeState extends State<Home>{
+
+  test()async{
+    for(String cityName in citys){
+      var model = getIt<DataServices>(param1: cityName, param2: '');
+    AirDataModel tester = await model.getCityData();
+    print('Şehir: $cityName ' + 'Co: ${tester.co} '  + 'Pm10: ${tester.pm10} ' + 'pm25: ${tester.pm25} ' );
+    }
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: test,
+      ),
 
     );
   }
