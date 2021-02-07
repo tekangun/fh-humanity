2f440f07e2062a763d350d559306e5b548e7d513
tekangun <tekan.gun@gmail.com>
2021-02-07 16:13:50 +0300

    tekan

--- lib/screens/splash_page.dart
+++ lib/screens/splash_page.dart
@@ -1,47 +1,42 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
 import 'package:hive/hive.dart';
 import 'package:humantiy/screens/intro.dart';
 import 'package:humantiy/screens/nav_bar/bottom_nav_bar.dart';
 
-
-
-
 class SplashPage extends StatefulWidget {
-
   @override
   State<StatefulWidget> createState() => SplashPageState();
 }
 
-class SplashPageState extends State<SplashPage>{
-
-
-
-
-  Future welcomeScreen() async{
+class SplashPageState extends State<SplashPage> {
+  Future welcomeScreen() async {
     await Hive.openBox('loginState');
     var box = await Hive.box('loginState');
     var status = await box.get('loginStatus');
     Timer(Duration(seconds: 3), () {
-      if(status == 1){
-        Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => BottomNavBarPage()), (route) => false);
-      }
-      else {
-        Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => IntroScreen()), (route) => false);
+      if (status == 1) {
+        Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(builder: (context) => BottomNavBarPage()),
+            (route) => false);
+      } else {
+        Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(builder: (context) => IntroScreen()),
+            (route) => false);
       }
     });
   }
 
-
   @override
   void initState() {
+    SystemChrome.setEnabledSystemUIOverlays([]);
     welcomeScreen();
     super.initState();
   }
-
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -54,13 +49,14 @@ class SplashPageState extends State<SplashPage>{
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Container(
+                decoration: BoxDecoration(shape: BoxShape.circle),
                 child: Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Image.asset(
-                    'assets/images/0_50.png',
-                    width: size.width * 0.4,
-                  ),
-                ),
+                    padding: EdgeInsets.all(8.0),
+                    child: CircleAvatar(
+                      radius: 80,
+                      backgroundColor: Colors.white,
+                      backgroundImage: AssetImage('assets/images/0_50.png'),
+                    )),
               ),
             ],
           ),
@@ -70,10 +66,12 @@ class SplashPageState extends State<SplashPage>{
         width: size.width,
         height: 40,
         color: Colors.white,
-        child: Text('Humantiy',style: TextStyle(fontSize: 14,color: Colors.black),textAlign: TextAlign.center,),
+        child: Text(
+          'Humantiy',
+          style: TextStyle(fontSize: 14, color: Colors.black),
+          textAlign: TextAlign.center,
+        ),
       ),
     );
   }
-
-
 }
