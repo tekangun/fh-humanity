ba690049ff2d0ee44232714a9b2f24052ea23bcd
berkay <berkayoruc@hotmail.com>
2021-02-07 15:38:27 +0300

    intro init

---
+++ lib/screens/intro.dart
@@ -0,0 +1,190 @@
+import 'package:flutter/material.dart';
+import 'package:intro_slider/intro_slider.dart';
+import 'package:intro_slider/slide_object.dart';
+
+class IntroScreen extends StatefulWidget {
+  IntroScreen({Key key}) : super(key: key);
+
+  @override
+  _IntroScreenState createState() => _IntroScreenState();
+}
+
+class _IntroScreenState extends State<IntroScreen> {
+  List<Slide> slides = [];
+
+  Function goToTab;
+
+  @override
+  void initState() {
+    super.initState();
+    slides.add(
+      Slide(
+        title: 'SCHOOL',
+        styleTitle: TextStyle(
+            color: Color(0xff3da4ab),
+            fontSize: 30.0,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'RobotoMono'),
+        description:
+            'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.',
+        styleDescription: TextStyle(
+            color: Color(0xfffe9c8f),
+            fontSize: 20.0,
+            fontStyle: FontStyle.italic,
+            fontFamily: 'Raleway'),
+        pathImage: 'assets/0_50.png',
+      ),
+    );
+    slides.add(
+      Slide(
+        title: 'MUSEUM',
+        styleTitle: TextStyle(
+            color: Color(0xff3da4ab),
+            fontSize: 30.0,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'RobotoMono'),
+        description:
+            'Ye indulgence unreserved connection alteration appearance',
+        styleDescription: TextStyle(
+            color: Color(0xfffe9c8f),
+            fontSize: 20.0,
+            fontStyle: FontStyle.italic,
+            fontFamily: 'Raleway'),
+        pathImage: 'assets/51_100.png',
+      ),
+    );
+    slides.add(
+      Slide(
+        title: 'COFFEE SHOP',
+        styleTitle: TextStyle(
+            color: Color(0xff3da4ab),
+            fontSize: 30.0,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'RobotoMono'),
+        description:
+            'Much evil soon high in hope do view. Out may few northward believing attempted. Yet timed being songs marry one defer men our. Although finished blessing do of',
+        styleDescription: TextStyle(
+            color: Color(0xfffe9c8f),
+            fontSize: 20.0,
+            fontStyle: FontStyle.italic,
+            fontFamily: 'Raleway'),
+        pathImage: 'assets/101_150.png',
+      ),
+    );
+  }
+
+  void onDonePress() {
+    // Back to the first tab
+    goToTab(0);
+  }
+
+  void onTabChangeCompleted(index) {
+    // Index of current tab is focused
+  }
+
+  Widget renderNextBtn() {
+    return Icon(
+      Icons.navigate_next,
+      color: Color(0xffffcc5c),
+      size: 35.0,
+    );
+  }
+
+  Widget renderDoneBtn() {
+    return Icon(
+      Icons.done,
+      color: Color(0xffffcc5c),
+    );
+  }
+
+  Widget renderSkipBtn() {
+    return Icon(
+      Icons.skip_next,
+      color: Color(0xffffcc5c),
+    );
+  }
+
+  List<Widget> renderListCustomTabs() {
+    var tabs = <Widget>[];
+    for (var i = 0; i < slides.length; i++) {
+      var currentSlide = slides[i];
+      tabs.add(Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Container(
+          margin: EdgeInsets.only(bottom: 60.0, top: 60.0),
+          child: ListView(
+            children: <Widget>[
+              GestureDetector(
+                  child: Image.asset(
+                currentSlide.pathImage,
+                width: 200.0,
+                height: 200.0,
+                fit: BoxFit.contain,
+              )),
+              Container(
+                child: Text(
+                  currentSlide.title,
+                  style: currentSlide.styleTitle,
+                  textAlign: TextAlign.center,
+                ),
+                margin: EdgeInsets.only(top: 20.0),
+              ),
+              Container(
+                child: Text(
+                  currentSlide.description,
+                  style: currentSlide.styleDescription,
+                  textAlign: TextAlign.center,
+                  maxLines: 5,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                margin: EdgeInsets.only(top: 20.0),
+              ),
+            ],
+          ),
+        ),
+      ));
+    }
+    return tabs;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return IntroSlider(
+      // List slides
+      slides: slides,
+
+      // Skip button
+      renderSkipBtn: renderSkipBtn(),
+      colorSkipBtn: Color(0x33ffcc5c),
+      highlightColorSkipBtn: Color(0xffffcc5c),
+
+      // Next button
+      renderNextBtn: renderNextBtn(),
+
+      // Done button
+      renderDoneBtn: renderDoneBtn(),
+      onDonePress: onDonePress,
+      colorDoneBtn: Color(0x33ffcc5c),
+      highlightColorDoneBtn: Color(0xffffcc5c),
+
+      // Dot indicator
+      colorDot: Color(0xffffcc5c),
+      sizeDot: 13.0,
+      // typeDotAnimation: dotSliderAnimation.SIZE_TRANSITION,
+
+      // Tabs
+      listCustomTabs: renderListCustomTabs(),
+      backgroundColorAllSlides: Colors.white,
+      refFuncGoToTab: (refFunc) {
+        goToTab = refFunc;
+      },
+
+      // Show or hide status bar
+      shouldHideStatusBar: true,
+
+      // On tab change completed
+      onTabChangeCompleted: onTabChangeCompleted,
+    );
+  }
+}
